d7ae5b69206d23e6d3ea926ae8c28dff143f9f51
Fuad Hasan <fuadhasandipro08@gmail.com>
2026-03-03 21:06:19 +0600

    fix header fo home

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,6 @@ import Footer from "@/components/Footer";
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex min-h-screen flex-col font-sans">
-            <Header showLocations={true} />
             <main className="flex-1">{children}</main>
             <Footer />
         </div>
